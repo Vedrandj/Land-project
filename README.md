e02cac87ac1680f223078324146f71edb421a788
Vedran <82266699+Vedrandj@users.noreply.github.com>
2022-09-12 22:34:09 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+# Landing Page Project
+
+## Table of Contents
+
+- [Instructions](#instructions)
+
+## Instructions
+
+This project was mostly made in js, some elements were added in html and css. DOM functions were used, such as, for example, navbar was implemented via DOM. An active registration form was also included. The project is responsive for models on phones, tablets and desktops.
+#Land-project
---
+++ app.js
@@ -0,0 +1,133 @@
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
+
+/**
+ * Comments should be present at the beginning of each procedure and class.
+ * Great to have comments before crucial code sections within the procedure.
+*/
+
+/**
+ * Define Global Variables
+ * 
+*/
+
+  const sections = document.querySelectorAll('section');
+ 
+  const navLi = document.querySelectorAll('.navbar__menu ul li');
+  
+  
+/**
+ * End Global Variables
+ * Start Helper Functions
+ * 
+*/
+
+
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ * 
+*/
+
+// build the nav
+
+const ul = document.getElementById("navbar__list");
+document.querySelector("#navbar__list").innerHTML += '<li><a href="#section1">How we are doing it</a></li>, <li><a href="#section2">Showcase</a></li>,  <li><a href="#section3">Our Projects</a></li>, <li><a href="#section4">Testimonials</a></li>, <li><a href="#form">Subscribe</a></li> '
+
+document.querySelectorAll("nav a").forEach(function(link, index) {
+	link.addEventListener('click',function(event){
+    event.preventDefault()
+    const section = document.querySelector(link.getAttribute('href'));
+    section.scrollIntoView({behavior:"smooth", inline:"center", block:"center"})
+  })
+});
+
+
+// Add class 'active' to section when near top of viewport
+
+// function makeActive(){
+//   for (let i = 0; i< sections.length; i++) {
+//     console.log(sections[i])
+//     const box = sections[i].getBoundingClientRect();
+//     const id = sections[i].getAttribute('id');
+//       if (box.top <= 150 && box.bottom >= 150) {
+        
+//       // document.querySelector(`[href='#${id}']`).classList.add('active');
+//       } else {
+//       // document.querySelector(`[href='#${id}']`).classList.remove('active');
+//       }
+//    }
+// }
+// makeActive()
+function makeActive() {
+  sections.forEach ((section)=>{
+    const box = section.getBoundingClientRect();
+    const id = section.getAttribute('id');
+    const list = document.querySelector(`[href="#${id}"]`);
+    if (box.top <= 150 && box.bottom >= 150){
+      section.classList.add('your-active-class') 
+    } else {
+      section.classList.remove('your-active-class')
+    }
+  
+  })
+}
+makeActive()
+// Scroll to anchor ID using scrollTO event
+
+
+/**
+ * End Main Functions
+ * Begin Events
+ * 
+*/
+
+// Build menu 
+
+function makeActive() {
+  sections.forEach ((section)=>{
+    const box = section.getBoundingClientRect();
+    const id = section.getAttribute('id');
+    const list = document.querySelector(`[href="#${id}"]`);
+    if (box.top <= 150 && box.bottom >= 150){
+      section.classList.add('your-active-class') 
+      list.classList.add('active');
+    } else {
+      section.classList.remove('your-active-class')
+      list.classList.remove('active');
+    }
+  
+  })
+}
+makeActive()
+
+// Scroll to section on link click
+document.addEventListener('scroll', (e) => {
+  e.preventDefault();
+  makeActive();
+});
+
+
+const form = document.querySelector('form');
+form.addEventListener('submit',(event)=>{
+  event.preventDefault()
+  const thanks = document.createElement('p');
+  form.append(thanks);
+  thanks.textContent = "Thanks for submiting on our Page";
+  setTimeout(()=>{
+    thanks.remove()
+  },6000)
+})
